19637993f498d4e5c439f54071170af423b4452f
Bhanu-Pratap127 <bhanupartap1273@gmail.com>
2024-12-22 15:47:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ function insert_Row() {
 	const rowCount = table.rows.length;
 	const cell1=newRow.insertCell();
 	const cell2=newRow.insertCell();
-    cell1.textContent = rowCount;
+    // cell1.textContent = rowCount;
 	cell1.textContent="New Cell1";
 	cell2.textContent="New Cell2"
 	
 	
   
 }
-insert_Row();
+// insert_Row();
