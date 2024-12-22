190aa74424502e014dde7b129faa694422f2d293
Bhanu-Pratap127 <bhanupartap1273@gmail.com>
2024-12-22 15:42:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ function insert_Row() {
     //Write your code here
   const table=document.getElementById("sampleTable");
 	const newRow=table.insertRow(0);
+	const rowCount = table.rows.length;
 	const cell1=newRow.insertCell();
 	const cell2=newRow.insertCell();
-
+    cell1.textContent = rowCount;
 	cell1.textContent="New Cell1";
 	cell2.textContent="New Cell2"
 	
