08e1d00b39b93a72fc672600079ea535f04ad58b
Bhanu-Pratap127 <bhanupartap1273@gmail.com>
2024-12-22 15:35:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function insert_Row() {
     //Write your code here
-  
+  const table=document.getElementById("sampleTable");
+	const newRow=table.insertRow(0);
+	const cell1=newRow.insertCell();
+	const cell2=newRow.insertCell();
+
+	cell1.textContent="New Cell1";
+	cell2.textContent="New Cell2"
+	
+	
   
 }
+insert_Row();
